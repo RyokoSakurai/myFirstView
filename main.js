cd2bf49bcd499a3dbd2d9bbf8632ccff4b20c63e
RyokoSakurai <sakuraimusa@gmail.com>
2022-08-04 11:34:09 +0900

    リストの追加

--- main.js
+++ main.js
@@ -5,10 +5,27 @@ const app = new Vue({
       todos: []
     },
     methods: {
-
+      //Todo追加処理（インスタンスメソッド）
+      doAdd: function(event,value) {
+        //refで名前を付けた要素を参照、varは再代入可能な変数宣言
+        var comment = this.$refs.comment
+        //入力なければ何もしない
+        if(!comment.value.length) {
+          return
         }
+        //{ID,コメント,作業状態}
+        //のオブジェクトをtodosリストへpush
+        //stateはデフォが作業中=0で作成
+        this.todos.push({
+          id: todoStorage.uid++,
+          comment: comment.value,
+          state: 0
+        })
+        comment.value = ''
+      }
     }
-  )
+  }
+)
 
 // https://jp.vuejs.org/v2/examples/todomvc.html
 var STORAGE_KEY = 'todos-vuejs-demo'
